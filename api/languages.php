2bea845986c5ebd5114bf909e90050eb4923f23e
Joao <joaojkuligowski@gmail.com>
2024-10-15 19:33:19 -0300

    Vercel adapting

--- api/languages.php
+++ api/languages.php
@@ -1,5 +1,5 @@
 <?php
 if ($_GET['lang']) {
   header('Content-Type: application/json');
-  echo file_get_contents('lang/' . $_GET['lang'] . '.json');
+  echo require __DIR__ . '/public/languages/' . basename($_GET['lang'] . '.json');
 }
