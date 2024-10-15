425d961add90044f8042ae1052c900d14403b367
Joao <joaojkuligowski@gmail.com>
2024-10-15 19:49:41 -0300

    Vercel adapting

--- api/languages.php
+++ api/languages.php
@@ -1,5 +1,13 @@
 <?php
-header('Content-Type: application/json, charset=utf-8');
-if ($_GET['lang']) {
-  echo require __DIR__ . '/public/lang/' . basename($_GET['lang'] . '.json');
+header('Content-Type: application/json; charset=utf-8');
+
+$lang = $_GET['lang'] ?? 'default'; // Define um padrão se o lang não for fornecido
+$lang = preg_replace('/[^a-zA-Z]/', '', $lang); // Remove caracteres indesejados para segurança
+
+$filePath = __DIR__ . '/public/lang/' . $lang . '.json';
+
+if (file_exists($filePath)) {
+  echo file_get_contents($filePath);
+} else {
+  echo json_encode(['error' => 'Language file not found']);
 }
